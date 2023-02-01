330efcbc0e053b49f2d9a92824f5b5a568f55497
NataliaJeszke <nataliajeszke8@gmail.com>
2023-02-01 17:44:25 +0100

    initial commit with basic styling and two input boxes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import generalStyle from './style.module.css';
+import { InputValues } from './components/InputValues/InputValues';
+export function App(){
+    return <div>
+        <header className={generalStyle.header}>
+        <h1 className={generalStyle.headline}>Simple Calculator in React</h1>
+        </header>
+        <div>
+        <InputValues />
+        </div>
+    </div>
+}
---
+++ src/components/InputValues/InputValues.jsx
@@ -0,0 +1,13 @@
+import inputStyle from "./style.module.css";
+export function InputValues() {
+
+
+  return (
+    <div className={inputStyle.inputContainer}>
+      <h3>Type first value</h3>
+      <input id="value1" className={inputStyle.inputBox} type="number" />
+      <h3>Type second value</h3>
+      <input id="value2" className={inputStyle.inputBox} type="number" />
+    </div>
+  );
+}
