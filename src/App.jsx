6629b39b6afbb0f8745009d7c7a0d3b735c79dc1
NataliaJeszke <nataliajeszke8@gmail.com>
2023-02-01 22:52:07 +0100

    styling for body and inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import generalStyle from "./style.module.css";
 import { InputValues } from "./components/InputValues/InputValues";
-import { Operations } from "./components/Operations/Operations";
 
 export function App() {
-
-    const handleSum = ()=>{
-        console.log("suma")
-    }
         
   return (
     <div>
       <header className={generalStyle.header}>
         <h1 className={generalStyle.headline}>Simple Calculator in React</h1>
       </header>
-      <div>
+      <main>
         <InputValues />
-      </div>
+      </main>
     </div>
   );
 }
